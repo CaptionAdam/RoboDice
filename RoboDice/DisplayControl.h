bccbef5cafe2e82ab1dad0e46e042ce3d7678a6e
Adam K <adamwkuefler@proton.me>
2026-01-02 11:22:57 -0700

    BetterCode
    Major Code Improvents and more features

--- RoboDice/DisplayControl.h
+++ RoboDice/DisplayControl.h
@@ -1,5 +1,5 @@
 //OLED Stuff
-#include <SPI.h>
+// #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
--- RoboDice/RoboDice.ino
+++ RoboDice/RoboDice.ino
@@ -1,13 +1,12 @@
 //RoboDiceGoBrrrrrrr
+// Now Using SeedStudio Xaio ESP32-C6
 
 //OLED Stuff
 #include "DisplayControl.h"
 
-#define PICO_DEFAULT_I2C_SDA_PIN 6
-#define PICO_DEFAULT_I2C_SCL_PIN 7
-#define RollButton 15
-#define SplitButton 16
-#define SwitchButton 17
+#define RollButton D8
+#define SplitButton D7
+#define SwitchButton D9
 
 int roll = 0;
 int dVal = 0;
@@ -20,6 +19,7 @@ int iD2 = 0;
 int pastRolls[3];
 
 void setup() {
+  int ButState = digitalRead(RollButton);
 
   // Set pinModes
   pinMode(RollButton, INPUT_PULLUP);
@@ -36,9 +36,22 @@ void setup() {
 
   // BootScreen
   display.display();
+  // DrawBootScreen();
   drawbitmap();
+
+
+  while (FirstRoll == 1) {
+    FirstRollSplash();  //Pre-First Roll Splash
+
+    ButState = digitalRead(RollButton);
+    if (ButState == 0) {
+      randomSeed(micros());
+      delay(30);
+      FirstRoll = 0;
+      // roll = 1;
+    }
+  }
   RollModeSplash();
-  FirstRollSplash();  //Pre-First Roll Splash
 }
 
 void loop() {
@@ -46,18 +59,6 @@ void loop() {
 
   // Set Random Seed to Time in micro secinds since program start
 
-
-
-  while (FirstRoll == 1) {
-    int ButState = digitalRead(RollButton);
-    if (ButState == 0) {
-      randomSeed(micros());
-      delay(30);
-      FirstRoll = 0;
-      roll = 1;
-    }
-    Serial.println(micros());
-  }
 
   ButState = digitalRead(RollButton);
   if (ButState == 0) {
@@ -78,51 +79,62 @@ void loop() {
     RollModeSplash();
   }
 
-  switch (RollMode) {
-    case 0:
-      if (roll == 1) {
+  if (roll == 1) {
+    switch (RollMode) {
+      case 0:
+        // Roll 1 D6
         dVal = random(1, 7);
         Serial.println(dVal);
-        screenPrint = 1;
-        roll = 0;
         OutputMode = 0;
-        Rolling();
         Serial.println("1*6");
-      }
-      break;
-    case 1:
-      // Roll 2 D6
-      if (roll == 1) {
+        break;
+
+      case 1:
+        // Roll 2 D2
+        if( dVal == 0) OutputMode = 0;
         iD1 = random(1, 7);
         iD2 = random(1, 7);
         dVal = iD1 + iD2;
-        roll = 0;
         Serial.println(dVal);
-        screenPrint = 1;
-        Rolling();
         Serial.println("2*6");
-      }
-      break;
-    case 2:
-      if (roll == 1) {
+        break;
+
+      case 2:
+        // Roll 1 D12
         dVal = random(1, 13);
         Serial.println(dVal);
-        roll = 0;
-        screenPrint = 1;
         OutputMode = 0;
-        Rolling();
         Serial.println("1*12");
-      }
-      break;
-    default:
-      Serial.println("It's Broke");
-      break;
+        break;
+
+      case 3:
+        // Roll 1 D4
+        dVal = random(1, 5);
+        Serial.println(dVal);
+        OutputMode = 0;
+        Serial.println("1*4");
+        break;
+
+      case 4:
+        // Roll 1 D4 for NESW
+        dVal = random(1, 5);
+        Serial.println(dVal);
+        OutputMode = 2;
+        Serial.println("NESW");
+        break;
+
+      default:
+        Serial.println("It's Broke");
+        break;
+    }
+    roll = 0;
+    if (dVal |= 0) screenPrint = 1;
+    Rolling();
   }
 
+  if (dVal |= 0) screenOut();
 
-  screenOut();
-
-  //screenOut();
+  // screenOut();
 }
 
 void ChangeMode() {
@@ -137,16 +149,9 @@ void ChangeMode() {
 }
 
 void RollModeChange() {
+  dVal = 0;
   RollMode += 1;
-  if (RollMode >= 3) RollMode = 0;
-  // if (RollMode == 0) {
-  //   Serial.println("1*6");
-  // }
-  // if (RollMode == 1) {
-  //   Serial.println("2*6");
-  // }
-  // if (RollMode == 2) {
-  //   Serial.println("1*12");
+  if (RollMode >= 5) RollMode = 0;
   Serial.println(RollMode);
 }
 
@@ -177,36 +182,44 @@ void FirstRollSplash() {
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);  // Start at top-left corner
   display.println("Press To");
-  display.println("Roll");
+  display.println("Set Seed");
+  display.println(micros());
   display.display();
 }
 
 void screenOut() {
-  if ((screenPrint == 1) && (OutputMode == 0)) {
-    display.clearDisplay();
-    display.setTextSize(8);
-    display.setTextColor(SSD1306_WHITE);
-    if (dVal <= 9) display.setCursor(40, 0);  // Start at top-left corner
-    if (dVal >= 10) display.setCursor(15, 0);
-    display.println(dVal);
-    display.setCursor(0, 0);  // Start at top-left corner
-    display.setTextSize(2);
-    display.display();
+  display.clearDisplay();
+  display.setTextSize(8);
+  display.setTextColor(SSD1306_WHITE);
+  switch (OutputMode) {
+    case 0:
+      // Single Number
+      if (dVal <= 9) display.setCursor(40, 0);  //
+      if (dVal >= 10) display.setCursor(15, 0);
+      display.println(dVal);
+      break;
+    case 1:
+      // 2 Single Digit
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.println(iD1);
+      display.setCursor(80, 0);
+      display.print(iD2);
+      display.setTextSize(4);
+      display.setCursor(50, 18);
+      display.print("+");
+      break;
+    case 2:
+      // DIRECTIONAL
+      display.setCursor(40, 0);
+      if (dVal == 1) display.println("N");
+      if (dVal == 2) display.println("E");
+      if (dVal == 3) display.println("S");
+      if (dVal == 4) display.println("W");
+      break;
+    default:
+      break;
   }
-
-  if ((screenPrint == 1) && (OutputMode == 1)) {
-    display.clearDisplay();
-    display.setTextSize(8);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);  // Start at top-left corner
-    display.println(iD1);
-    display.setCursor(80, 0);
-    display.print(iD2);
-    display.setTextSize(4);
-    display.setCursor(50, 18);
-    display.print("+");
-    display.display();
-  }
+  display.display();
 }
 
 
@@ -214,18 +227,31 @@ void RollModeSplash() {
   display.clearDisplay();
   display.setTextSize(5);
   display.setTextColor(SSD1306_WHITE);
-  if (RollMode == 0) {
-    display.setCursor(0, 0);  // Start at top-left corner
-    display.print("1*6");
+  switch (RollMode) {
+    case 0:
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print("1*6");
+      break;
+    case 1:
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print("2*6");
+      break;
+    case 2:
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print("1*12");
+      break;
+    case 3:
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print("1*4");
+      break;
+    case 4:
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print("NESW");
+      break;
+    default:
+      break;
   }
-  if (RollMode == 1) {
-    display.setCursor(0, 0);  // Start at top-left corner
-    display.print("2*6");
-  }
-  if (RollMode == 2) {
-    display.setCursor(0, 0);  // Start at top-left corner
-    display.print("1*12");
-  }
+
   display.display();
-  delay(1000);
+  delay(100);
 }
